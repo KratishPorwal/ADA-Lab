5c87215af7d68c6de203d1a15edc9af46c281be0
KratishPorwal <kratishporwal.cs23@bmsce.ac.in>
2025-03-25 09:24:19 +0530

    Add files via upload

---
+++ Lab 4/Quick sort.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Function to swap two elements
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Partition function
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // Choosing the last element as pivot
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+// QuickSort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    clock_t start, end;
+    double time_taken;
+
+    start = clock(); // Start time
+    quickSort(arr, 0, n - 1);
+    end = clock(); // End time
+
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+    printf("Time taken: %f seconds\n", time_taken);
+
+    return 0;
+}
